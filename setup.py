61a41418779c36aaa57138b1f7ec02be1cbf55fb
Bartek Cupiał <bartlomiej.cupial@gmail.com>
2025-07-01 19:41:04 +0000

    nle language wrapper

--- setup.py
+++ setup.py
@@ -79,6 +79,11 @@ packages = [
     "nle.agent",
     "nle.scripts",
     "nle.tests",
+    "nle.language_wrapper.agents",
+    "nle.language_wrapper.agents.sample_factory",
+    "nle.language_wrapper.scripts",
+    "nle.language_wrapper.tests",
+    "nle.language_wrapper.wrappers",
 ]
 
 entry_points = {
